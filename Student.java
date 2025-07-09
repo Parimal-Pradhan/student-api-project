2518278587c38bfe00acc745460fdd0d1dbd0adb
Parimal Pradhan <parimalpradhan007@gmail.com>
2025-07-09 20:16:23 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,17 @@
+package com.cloudnixtech.student.model;
+
+public class Student {
+    private String name;
+    private String email;
+    private int age;
+
+    public Student() {}
+
+    public Student(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Getters and Setters
+}
